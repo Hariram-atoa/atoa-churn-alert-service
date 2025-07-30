01e9d6e71dd5566d8601e8ea0b05adb2c8ccf433
Hariram-atoa <hariram@paywithatoa.co.uk>
2025-07-30 20:28:20 +0530

    feat: Integrate Swagger for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // Load environment variables from .env file
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Atoa Churn Alert Service')
+    .setDescription('API documentation for the Atoa Churn Alert Service')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 8089);
 }
 bootstrap();
